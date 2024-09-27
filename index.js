5508a4fb2b302df4ca7cbeb0f081a5586d2abb5f
Aman Bhondge <amanbhondge@gmail.com>
2024-09-27 19:08:59 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+let imageEl = document.getElementById("image");
+
+function springFn() {
+    imageEl.src = "image/Spring.png";
+}
+
+function autumnFn() {
+    imageEl.src = "image/Autumn.png";
+}
+
+function summerFn() {
+    imageEl.src = "image/Summer.png";
+}
+
+function winterFn() {
+    imageEl.src = "image/Winter.png";
+}
